096567344e2e7918593e29568b7a9aee2a6f1fc8
Lyheng <senglyheng2001@gmail.com>
2024-02-14 12:53:56 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import GiftScreen from "@/screen/Gift/GiftScreen";
+import HomeScreen from "@/screen/Home/Home";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <GiftScreen />
+  );
+}
---
+++ src/screen/Gift/GiftScreen.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import "./gift_screen.scss"
+const GiftScreen = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="glass"></div>
+        <div className="thorns">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="glow"></div>
+        <div className="rose-leaves">
+          <div></div>
+          <div></div>
+        </div>
+        <div className="rose-petals">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="sparkles">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+      <div className='aaa'>
+        <div className='bbb'>Happy Valentine's Day Babe ❤️</div>
+      </div>
+
+    </>
+
+  )
+}
+
+export default GiftScreen
